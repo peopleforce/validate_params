2dec4ab1a6fffbbc17bdebe7909a63286c375e6c
denisc <denis.cherevatenko@peopleforce.io>
2024-09-24 22:54:06 +0300

    feature: add support proc value for min and max validations

---
+++ spec/fixtures/controllers/with_proc_min_and_max_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "base_test_controller"
+
+class WithProcMinAndMaxController < BaseTestController
+  validate_params_for :index, format: :json do |p|
+    p.param :date_of_birth, Date, min: proc { Date.new(2020, 1, 1) }, max: proc { Date.new(2025, 1, 1) }
+    p.param :created_at, DateTime, min: proc { DateTime.new(2020, 1, 1, 12, 30) }, max: proc { DateTime.new(2025, 1, 1, 12, 30) }
+    p.param :quantity, Integer, min: proc { 1 }, max: proc { 10 }
+  end
+
+  def index
+    "success"
+  end
+end
